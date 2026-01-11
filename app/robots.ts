94ca19185ffcc560c67876ef37466d49af4ecdd9
Mbogneng-Junior <mbonengj@gmail.com>
2026-01-11 08:09:42 +0100

    augmentation de la visibilité google 2

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://mbogneng-junior.vercel.app' // Remplace par ton vrai domaine
+  const baseUrl = 'https://portfolio.mbognengjunior.me' // Remplace par ton vrai domaine
 
   return {
     rules: {
